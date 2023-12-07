9d7240df0ffd33ff6184a4d3acd805d249bc82c9
Melledodo <mkaagaard@hotmail.com>
2023-12-07 21:10:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ document.addEventListener("DOMContentLoaded", function () {
     temperatureLoop();
 });
 //signe slut
+// Michelle
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'bar',
