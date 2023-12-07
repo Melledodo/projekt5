1f3d13e6ad973336ea191e3f3aace14f53b9e54c
Melledodo <mkaagaard@hotmail.com>
2023-12-07 09:16:07 +0100

    x

--- script.js
+++ script.js
@@ -76,5 +76,11 @@ const myChart = new Chart(ctx, {
 const målinger = document.getElementById('målinger');
 målinger.addEventListener('change', grafTracker);
 function grafTracker(){
+<<<<<<< Updated upstream
 
 }
+=======
+    console.log(målinger.value.split(','));
+    målinger.value.split(',');
+}
+>>>>>>> Stashed changes
